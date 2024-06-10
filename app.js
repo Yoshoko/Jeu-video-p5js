c441c04fe37ea9d4b81769b60c3af9bc3cacf75b
AymericMartinez <aymeric.martinez@esd-mail.com>
2024-06-10 16:56:45 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(1420, 800);
+    background('grey');
+
+
+
+    joueur = new Sprite(35, 200);
+    joueur.width = 30;
+    joueur.height = 40;
+}
+
+function draw() {
+
+}
